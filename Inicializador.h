e1a62eb21acc3c90a29b90a68181c1fb0cb1166a
AndresBolanos <ronald-9797@hotmail.com>
2018-05-20 08:27:32 -0700

    Delete Inicializador.h

--- Inicializador.h
+++
@@ -1,9 +0,0 @@
-#ifndef SHAREDMEMORY_INICIALIZADOR_H
-#define SHAREDMEMORY_INICIALIZADOR_H
-
-#include "imports.h"
-
-
-void Create_Memory(char * memory_name, int memory_key, int lines_memory);
-
-#endif 
